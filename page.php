6301fe7036c39cd7df9d7b6762848d9c5b1470ee
jared <jtruscott78@gmail.com>
2018-08-11 08:56:14 -0400

    set up link side menu

--- page.php
+++ page.php
@@ -1,11 +1,11 @@
 
 <?php
-  get_header();
-  while(have_posts()){
-    the_post(); ?>
+get_header();
+while (have_posts()) {
+  the_post(); ?>
 
   <div class="page-banner">
-    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri ('images/ocean.jpg')?>);"></div>
+    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg') ?>);"></div>
     <div class="page-banner__content container container--narrow">
       <h1 class="page-banner__title"><?php the_title() ?></h1>
       <div class="page-banner__intro">
@@ -16,28 +16,51 @@
   <div class="container container--narrow page-section">
 
   <?php
-    $theParent = wp_get_post_parent_id(get_the_ID());
-    if  ($theParent) { ?>
+  $theParent = wp_get_post_parent_id(get_the_ID());
+  if ($theParent) { ?>
       <div class="metabox metabox--position-up metabox--with-home-link">
         <p><a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent); ?></a> <span class="metabox__main"><?php the_title() ?></span></p>
       </div>
-   <?php }
-  ?>
+   <?php
 
+}
+?>
 
-    <!--
-    <div class="page-links">
-      <h2 class="page-links__title"><a href="#">About Us</a></h2>
-      <ul class="min-list">
-        <li class="current_page_item"><a href="#">Our History</a></li>
-        <li><a href="#">Our Goals</a></li>
-      </ul>
-    </div>
-    -->
+  <?php
+  //test if current page has children and hide links if no children
+  $checkForChildren = get_pages([
+    'child_of' => get_the_ID()
+  ]);
+  // if has children create menu links
+  if ($theParent or $checkForChildren) {
+    ?>
+  <div class="page-links">
+    <h2 class="page-links__title"><a href="<?php echo get_permalink($theParent) ?>"><?php echo get_the_title($theParent) ?></a></h2>
+    <ul class="min-list">
+      <?php
+      // get ID for parent if page is child else get page ID
+      $findChildrenOf = $theParent ? $theParent : get_the_ID();
+
+      wp_list_pages([
+        'title_li' => null,
+        'child_of' => $findChildrenOf,
+        'sort_column' => 'menu_order'
+      ]);
+      ?>
+    </ul>
+  </div>
+
+<?php
+
+} ?>
+
     <div class="generic-content">
       <?php the_content(); ?>
     </div>
   </div>
+
    <?php get_footer(); ?>
 
-  <?php }
+  <?php
+
+}
